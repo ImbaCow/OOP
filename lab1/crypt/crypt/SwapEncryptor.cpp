dcac8839ca11751ba6693acbc2c6648a035000d9
Егор <e.iambakov@gmail.com>
2019-03-14 11:45:12 +0300

    Исправления по замечаниям 1 лабы

--- lab1/crypt/crypt/SwapEncryptor.cpp
+++ lab1/crypt/crypt/SwapEncryptor.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "SwapEncryptor.h"
 
-uint8_t swapBits(uint8_t byte)
+uint8_t SwapBits(uint8_t byte)
 {
 	uint8_t part1 = (byte & 0b10000000) >> 2;
 	uint8_t part2 = (byte & 0b00000111) << 2;
@@ -10,7 +10,7 @@ uint8_t swapBits(uint8_t byte)
 	return part1 | part2 | part3;
 }
 
-uint8_t swapBitsBack(uint8_t byte)
+uint8_t SwapBitsBack(uint8_t byte)
 {
 	uint8_t part1 = (byte & 0b00100000) << 2;
 	uint8_t part2 = (byte & 0b00011100) >> 2;
@@ -19,37 +19,41 @@ uint8_t swapBitsBack(uint8_t byte)
 	return part1 | part2 | part3;
 }
 
-uint8_t encodeByte(uint8_t byte, uint8_t key)
+uint8_t EncodeByte(uint8_t byte, uint8_t key)
 {
 	byte ^= key;
-	return swapBits(byte);
+	return SwapBits(byte);
 }
 
-uint8_t decodeByte(uint8_t byte, uint8_t key)
+uint8_t DecodeByte(uint8_t byte, uint8_t key)
 {
-	byte = swapBitsBack(byte);
+	byte = SwapBitsBack(byte);
 	return byte ^ key;
 }
 
-void encode(const std::function<uint8_t(uint8_t, uint8_t)>& modeFunc, std::istream& input, std::ostream& output, uint8_t key)
+void Encode(std::istream& input, std::ostream& output, const std::function<uint8_t(uint8_t)>& modeFunc)
 {
 	uint8_t byte;
 	while (input.read(reinterpret_cast<char*>(&byte), sizeof(byte)))
 	{
-		uint8_t encodedByte = modeFunc(byte, key);
+		uint8_t encodedByte = modeFunc(byte);
 		output.write(reinterpret_cast<char*>(&encodedByte), sizeof(encodedByte));
 	}
 }
 
-void encryptStream(const std::string& mode, std::istream& input, std::ostream& output, uint8_t key)
+void EncryptStream(const std::string& mode, std::istream& input, std::ostream& output, uint8_t key)
 {
 	if (mode == "crypt")
 	{
-		encode(encodeByte, input, output, key);
+		Encode(input, output, [key](uint8_t byte) {
+			return EncodeByte(byte, key);
+		});
 	}
 	else if (mode == "decrypt")
 	{
-		encode(decodeByte, input, output, key);
+		Encode(input, output, [key](uint8_t byte) {
+			return DecodeByte(byte, key);
+		});
 	}
 	else
 	{
--- lab1/crypt/crypt/SwapEncryptor.h
+++ lab1/crypt/crypt/SwapEncryptor.h
@@ -1,9 +1,9 @@
 #pragma once
 
-uint8_t swapBits(uint8_t byte);
-uint8_t swapBitsBack(uint8_t byte);
+uint8_t SwapBits(uint8_t byte);
+uint8_t SwapBitsBack(uint8_t byte);
 
-uint8_t encodeByte(uint8_t byte, uint8_t key);
-uint8_t decodeByte(uint8_t byte, uint8_t key);
+uint8_t EncodeByte(uint8_t byte, uint8_t key);
+uint8_t DecodeByte(uint8_t byte, uint8_t key);
 
-void encryptStream(const std::string& mode, std::istream& input, std::ostream& outpit, uint8_t key);
+void EncryptStream(const std::string& mode, std::istream& input, std::ostream& outpit, uint8_t key);
